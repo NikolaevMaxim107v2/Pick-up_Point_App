dde1ee516a456e2544a673f700957293df773e91
NikolaevMD <NikolaevMD@309D-3>
2024-03-11 10:12:25 +0700

    Little UI changes

--- Model/Product.cs
+++ Model/Product.cs
@@ -12,15 +12,17 @@ namespace Pick_up_Point_App.Model
         private int _id;
         private string _name;
         private int _productTypeId;
+        private string _productDesc;
         private float _price;
         //private int _relOrderID;
 
 
-        public Product(int id, string name, int productTypeId, float price)
+        public Product(int id, string name, int productTypeId, string productDesc, float price)
         {
             Id = id;
             Name = name;
             ProductTypeId = productTypeId;
+            ProductDesc = productDesc;
             Price = price;
             //RelOrderId = relOrderID;
         }
@@ -28,6 +30,7 @@ namespace Pick_up_Point_App.Model
         public int Id { get { return _id; } set { _id = value; OnPropertyChanged(nameof(Id)); } }
         public string Name { get { return _name; } set { _name = value; OnPropertyChanged(nameof(Name)); } }
         public int ProductTypeId { get { return _productTypeId; } set { _productTypeId = value; OnPropertyChanged(nameof(ProductTypeId)); } }
+        public string ProductDesc { get { return _productDesc; } set { _productDesc = value; OnPropertyChanged(nameof(ProductDesc)); } }
         public float Price { get { return _price; } set { _price = value; OnPropertyChanged(nameof(Price)); } }
         //public int RelOrderId { get { return _relOrderID; } set { _relOrderID = value; OnPropertyChanged(nameof(RelOrderId)); } }
     }
